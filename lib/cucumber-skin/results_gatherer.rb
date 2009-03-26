25d5b70b92429da7f8217be9e2ae6b6500b40f12
Tom ten Thij <git@tomtenthij.nl>
2009-03-26 02:02:17 +0000

    Start on results gatherer

---
+++ lib/cucumber-skin/results_gatherer.rb
@@ -0,0 +1,6 @@
+require 'cucumber'
+
+module CucumberSkin
+  class ResultsGatherer < Cucumber::Ast::Visitor
+  end
+end
---
+++ spec/cucumber-skin/results_gatherer_spec.rb
@@ -0,0 +1,16 @@
+# spec/cucumber-skin/cucumber-skin/results_gatherer_spec.rb
+# $Id$
+
+# Require the spec helper relative to this file
+require File.join(File.dirname(__FILE__), %w[ .. spec_helper])
+
+# No need to type CucumberSkin:: before each call
+include CucumberSkin
+
+describe ResultsGatherer do
+  it "should inherit from the cucumber ast visitor" do
+    ResultsGatherer.new(:step_mother).should be_a(Cucumber::Ast::Visitor)
+  end
+end
+
+# EOF
