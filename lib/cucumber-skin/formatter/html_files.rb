615c2d2e522e523bf1d287ff60621a50b2fae6fe
Tom ten Thij <git@tomtenthij.nl>
2009-03-26 10:16:26 +0000

    Minimal code to be able to use CucumberSkin::Formatter::HtmlFiles as a formatter using cucumber-skin command (involves a hack specific for local machine for now)

--- lib/cucumber-skin/formatter/html_files.rb
+++ lib/cucumber-skin/formatter/html_files.rb
@@ -1,7 +1,17 @@
+require 'cucumber'
 
 module CucumberSkin
   module Formatter
     class HtmlFiles
+      attr_accessor :options
+
+      def initialize(step_mother, io, options)
+        @options = options
+      end
+
+      def visit_features(features)
+        puts "Visiting features"
+      end
     end
   end
 end
--- spec/cucumber-skin/formatter/html_files_spec.rb
+++ spec/cucumber-skin/formatter/html_files_spec.rb
@@ -4,10 +4,13 @@
 # Require the spec helper relative to this file
 require File.join(File.dirname(__FILE__), %w[ .. .. spec_helper])
 
-# No need to type CucumberSkin:: before each call
-include CucumberSkin
+# No need to type CucumberSkin::Formatter before each call
+include CucumberSkin::Formatter
 
 describe HtmlFiles do
+  it "should implement visit_features" do
+    HtmlFiles.new(:step_mother, :io, {}).should respond_to(:visit_features)
+  end
 end
 
 # EOF
